fdca330bdd89dceb353df215cb2add15ef187a9a
Chinh Le <quocchinh.le@2359media.com>
2015-09-23 19:04:45 +0800

    initial implementation

--- CLExpandableTableView/Library/CLExpandableTableView.m
+++ CLExpandableTableView/Library/CLExpandableTableView.m
@@ -8,14 +8,105 @@
 
 #import "CLExpandableTableView.h"
 
+typedef NS_ENUM(NSInteger, SectionState) {
+    SectionStateCollapsed,
+    SectionStateLoading,
+    SectionStateExpanded
+};
+
+
+
+@interface CLExpandableTableView()
+<
+UITableViewDataSource,
+UITableViewDelegate
+>
+
+@property (nonatomic,strong) UITableView *tableView;
+
+@end
+
 @implementation CLExpandableTableView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark -
+#pragma mark - Init
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        [self commonInit];
+    }
+    
+    return self;
 }
-*/
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+- (void)commonInit
+{
+    [self setupTableView];
+}
+
+- (void)setupTableView
+{
+    self.tableView = [[UITableView alloc] init];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    [self positionTableView];
+}
+
+- (void)positionTableView
+{
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addSubview:self.tableView];
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[tableView]|" options:0 metrics:nil views:@{@"tableView":self.tableView}]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[tableView]|" options:0 metrics:nil views:@{@"tableView":self.tableView}]];
+}
+
+
+
+#pragma mark -
+#pragma mark - Public methods
+
+- (void)refreshSection:(NSInteger)section
+{
+    
+}
+
+
+
+
+#pragma mark -
+#pragma mark - TableView Delegate and DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [self.dataSource numberOfSectionsInExpandableTableView:self];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [self.dataSource expandableTableView:self cellForRowAtIndexPath:indexPath];
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    return [self.dataSource expandableTableView:self viewForHeaderInSection:section];
+}
+
 
 @end
